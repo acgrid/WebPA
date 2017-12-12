a9c511ce318ac268c9439e3fd9b1653e9bde0301
acgrid <acgrid@gmail.com>
2017-12-12 15:08:14 +0800

    Improve webpack stuff and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const webpack = require('webpack'),
      path = require('path');
 
 let entry = {
-        console: ['./src/console.js'],
-        monitor: ['./src/monitor.js'],
-    };
+    console: ['./src/console.js'],
+    monitor: ['./src/monitor.js'],
+};
 
 if(process.env.NODE_ENV !== 'production'){
-    for(let item in entry){
-        entry[item].push('webpack-hot-middleware/client');
+    for(let entryName in entry){
+        entry[entryName].push('webpack-hot-middleware/client');
     }
 }
 
@@ -16,10 +16,17 @@ module.exports = {
     entry,
     output: {
         path: path.resolve(__dirname, "public/js"),
-        filename: '[name].bundle.js',
+        filename: '[name].js',
         publicPath: '/js',
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.includes("node_modules");
+            }
+        }),
     ]
 };
