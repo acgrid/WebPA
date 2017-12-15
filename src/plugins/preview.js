698ee5948866fd8ff78da8355c5b25e4cfc5fe1e
acgrid <acgrid@gmail.com>
2017-12-15 18:10:20 +0800

    Implement console preview window

--- src/plugins/preview.js
+++ src/plugins/preview.js
@@ -7,13 +7,37 @@ class Preview extends Plugin{
     }
     init(type, main, event){
         if(type === 'console'){
+            this.active = false;
             this.main = main;
             this.sandbox = new Sandbox($(`<div class="sandbox"></div>`), {preview: true});
+            const executor = () => {
+                if(!this.active){
+                    this.main.createWindow({
+                        theme:       'warning',
+                        headerTitle: '预览窗口',
+                        position:    {
+                            my:      "right-bottom",
+                            at:      "right-bottom",
+                            offsetX: 0,
+                            offsetY: 50
+                        },
+                        contentSize: {width:  '960px', height: '540px'},
+                        content:     this.sandbox.container.get(0),
+                        callback: () => {
+                            this.active = true;
+                        },
+                        onclosed: () => {
+                            this.active = false;
+                        }
+                    });
+                }
+            };
             // register stuff
             event.on("console.build", () => {
                 main.createIcon(($icon) => {
                     $icon.find("i").addClass("fa-tv");
                     $icon.find("p").text("预览窗口");
+                    $icon.click(executor);
                     return $icon;
                 });
             });
