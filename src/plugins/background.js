484a5e89d331653b0df98715bc325dd73b4fb404
acgrid <acgrid@gmail.com>
2017-12-15 21:00:51 +0800

    Add background with DOM fix

--- src/plugins/background.js
+++ src/plugins/background.js
@@ -1,7 +1,65 @@
-const DOM = require('./dom');
+const DOM = require('./dom'),
+    $ = require('jquery'),
+    Storage = require('../lib/storage');
+const EVENT_GLOBAL_UPDATE_URL = "console.global.plugin.background.update",
+    EVENT_SANDBOX_UPDATE_URL = "sandbox.background.image.update",
+    STORAGE_BACKGROUND_URL = "plugin.background.url";
 
-module.exports = class Background extends DOM{
+class Background extends DOM{
     static name(){
         return 'background';
     }
-};
+    constructor(options = {}){
+        super('background');
+        this.className = options.className || 'background';
+        this.$dom = $(`<div class="full"></div>`).addClass(this.className);
+    }
+    createDOM(){
+        return this.$dom;
+    }
+    myInit(type, main, event){
+        this.event = event;
+        event.on(EVENT_SANDBOX_UPDATE_URL, (data) => {
+            this.$dom.css("background-image", data.url ? `url(${data.url})` : "");
+        });
+        if(type === 'console'){
+            this.url = Storage.get(STORAGE_BACKGROUND_URL, 'http://127.0.0.1:8080/mylive2017/KV.png');
+            this.$form = $(`<form><input type="url" class="block" id="background-image-url" /><button>设定</button></form>`);
+            this.$input = this.$form.find("input");
+            this.$form.find("button").click((ev) => {
+                ev.preventDefault();
+                this.setUrl(this.$input.val());
+            });
+            event.on(EVENT_GLOBAL_UPDATE_URL, (data) => {
+                const url = data.url || "";
+                this.$input.val(url);
+                Storage.set(STORAGE_BACKGROUND_URL, url);
+                event.emit(EVENT_SANDBOX_UPDATE_URL, {url});
+            });
+            event.on("console.build", () => {
+                main.createIcon(($icon) => {
+                    $icon.find("i").addClass("fa-image");
+                    $icon.find("p").text("背景图片");
+                    $icon.click(() => {
+                        main.openWindow('preview-setup', {
+                            theme:       'primary',
+                            headerTitle: '背景图片',
+                            position:    'center-top 0 30',
+                            contentSize: '450 250',
+                            content:     this.$form.get(0)
+                        });
+                    });
+                    return $icon;
+                });
+            });
+            this.setUrl(this.url);
+        }
+    }
+    setUrl(url){
+        this.event.emit(EVENT_GLOBAL_UPDATE_URL, {url});
+    }
+}
+
+module.exports = Background;
+module.exports.GlobalUpdateBackgroundUrl = EVENT_GLOBAL_UPDATE_URL;
+module.exports.SandboxUpdateBackgroundUrl = EVENT_SANDBOX_UPDATE_URL;
--- src/plugins/dom.js
+++ src/plugins/dom.js
@@ -1,17 +1,23 @@
 const Plugin = require('./base');
 
 module.exports = class DOM extends Plugin{
-    constructor(layer, callback){
+    constructor(layer){
         super();
         this.layer = layer;
-        this.callback = callback;
     }
     static name(){
         return 'dom';
     }
     init(type, main, event){
         event.on("sandbox.create", (sandbox) => {
-            sandbox.create(this.layer, this.callback);
+            sandbox.create(this.layer, this.createDOM.bind(this, [type, main, event]));
         });
+        this.myInit(type, main, event);
+    }
+    createDOM(type, main, event){
+        console.warn("Base version of DOM is invoked!", this.layer, event);
+    }
+    myInit(type, main, event){
+        console.warn("Base version of DOM is invoked!", this.layer, event);
     }
 };
