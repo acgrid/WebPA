8333f2c02aeea0e7d5183bb8a2d5e998505e18e0
acgrid <acgrid@gmail.com>
2017-12-16 18:54:42 +0800

    Remove unused code

--- src/lib/monitor.js
+++ src/lib/monitor.js
@@ -2,18 +2,6 @@ const $ = require('jquery'),
     Main = require('./main'),
     ev = require('../lib/event');
 
-/*$(function(){
-    const v = document.getElementById('playback');
-    const canvas = document.getElementById('overlay');
-    const context = canvas.getContext('2d');
-
-    canvas.width = v.clientWidth;
-    canvas.height = v.clientHeight;
-    context.strokeStyle = 'white';
-    context.strokeRect(0, 0, v.clientWidth, v.clientHeight);
-    context.strokeText('TEST3', 10, 10, 100);
-});*/
-
 class Monitor extends Main{
     constructor(channel, sandbox, plugins = []){
         super('monitor', plugins, {channel, sandbox}, (ev) => {
