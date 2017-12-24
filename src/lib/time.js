5f2e72c0ebebc284dfd218f18a1db528be79d786
acgrid <acgrid@gmail.com>
2017-12-24 21:05:59 +0800

    Improve time formatter utility

--- src/lib/time.js
+++ src/lib/time.js
@@ -1,8 +1,18 @@
+function join(...params){
+    return params.map(param => {
+        return param >= 0 && param < 10 ? `0${param}` : param;
+    }).join(':');
+}
 module.exports = {
     secondsToMS(seconds){
         seconds = parseInt(seconds);
         if(isNaN(seconds)) return '-';
-        return `${Math.floor(seconds / 60)}:${seconds % 60}`;
+        return join(Math.floor(seconds / 60), seconds % 60);
+    },
+    secondsToHMS(seconds){
+        seconds = parseInt(seconds);
+        if(isNaN(seconds)) return '-';
+        return join(Math.floor(seconds / 3600), Math.floor((seconds % 3600) / 60), seconds % 60);
     },
     msToSeconds(ms){
         const time = ms.split(':');
